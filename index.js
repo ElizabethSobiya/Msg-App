32c89f049a58656cd9aea10d4e9a81c1da3e6820
siril <sarockia@asu.edu>
2022-07-24 15:30:02 +0530

    socket changes

--- index.js
+++ index.js
@@ -9,16 +9,31 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html")
 } )
 
+let usersConnectedCount = 0 ;
+
 io.on("connection", (socket) => {
+    usersConnectedCount++;
+    io.emit("connection", usersConnectedCount)
     console.log(`user connected`);
     // socket.on("message", (msg) => {
     // //  console.log(msg);
     // io.emit('message', msg)
+
     // })
-    socket.on('message', () => {
-        console.log(`${userMessage} send by ${userName}`);
-        io.emit("message")
+    socket.on('message', (userMessage, userName) => {
+        // console.log(`${userMessage} send by ${userName}`);
+        io.emit("message", userMessage, userName)
     } )
+
+    socket.on("deleteMessage", (id) => {
+        io.emit("deleteMessage", id)
+    })
+
+    socket.on("disconnect", () => {
+        usersConnectedCount--;
+        io.emit("connection", usersConnectedCount)
+        // console.log(`user disconnected`)
+    })
 })
 
 // io.on("message", () => {
